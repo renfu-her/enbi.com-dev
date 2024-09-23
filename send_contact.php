a51caa6e7a50175cd2ab6bb12394af3ce66cf09d
Patrick Her <renfu.her@gmail.com>
2024-09-23 17:41:37 +0800

    chore: Update send_contact.php to remove unnecessary code

--- send_contact.php
+++ send_contact.php
@@ -2,6 +2,8 @@
 if (!isset($_SESSION)) {
 	session_start();
 }
+
+exit;
 echo '<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />';
 include "./admin1561318/common.func.php";
 ini_set('display_errors', 1);
